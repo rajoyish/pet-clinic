349a263348d79dc440a8ad1fc4e2d57607f463e5
Rajesh Budhathoki <rajoyish@gmail.com>
2024-05-08 15:41:59 +0545

    establish relationship and install filamentphp

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Appointment extends Model
 {
     use HasFactory;
+
+    public function pet(): BelongsTo
+    {
+        return $this->belongsTo(Pet::class);
+    }
 }
